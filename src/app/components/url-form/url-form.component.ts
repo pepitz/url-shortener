474786d03eb755fa186fe0892ba8179342c9188e
Petar Tsenev <petar.tzenev@gmail.com>
2024-11-07 23:32:42 +0100

    feat(url-form): create url-form structure

--- src/app/components/url-form/url-form.component.ts
+++ src/app/components/url-form/url-form.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { ControlComponent } from '../control/control.component';
 
 @Component({
   selector: 'app-url-form',
   standalone: true,
-  imports: [],
+  imports: [ControlComponent, FormsModule],
   templateUrl: './url-form.component.html',
   styleUrl: './url-form.component.scss',
 })
-export class UrlFormComponent {}
+export class UrlFormComponent {
+  onSubmit(inputUrl: string) {
+    console.log('onSubmit', inputUrl);
+  }
+}
