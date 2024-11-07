562ce70ec838fb7f9688934d567ce0e8a4bb3d31
Petar Tsenev <petar.tzenev@gmail.com>
2024-11-07 13:47:44 +0100

    feat(styles, structure): set initial structure; import component library and utilities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { UrlFormComponent } from './components/url-form/url-form.component';
+import { UrlTableComponent } from './components/url-table/url-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, UrlFormComponent, UrlTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/components/url-table/url-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-url-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './url-table.component.html',
+  styleUrl: './url-table.component.scss'
+})
+export class UrlTableComponent {
+
+}
