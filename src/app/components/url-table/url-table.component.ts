9694850e1a4248b8c6732df98b31f2d7ce40fb73
Petar Tsenev <petar.tzenev@gmail.com>
2024-11-10 23:12:26 +0100

    feat(url-table): add search

--- src/app/components/url-table/url-table.component.ts
+++ src/app/components/url-table/url-table.component.ts
@@ -1,13 +1,18 @@
 import { Component, inject, signal } from '@angular/core';
 
 import { QRCodeModule } from 'angularx-qrcode';
+
 import { TableModule } from 'primeng/table';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+
 import { UrlStore } from '../../state/url.store';
 
 @Component({
   selector: 'app-url-table',
   standalone: true,
-  imports: [TableModule, QRCodeModule],
+  imports: [TableModule, QRCodeModule, IconFieldModule, InputIconModule, InputTextModule],
   templateUrl: './url-table.component.html',
   styleUrls: ['./url-table.component.scss'],
 })
